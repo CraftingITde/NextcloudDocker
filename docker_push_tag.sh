b887abbecaf1ee3488f4e076158d5d6fe2d3dfba
KStrüssmann <kstruessmann@craftingit.de>
2019-08-25 14:43:04 +0200

    Updates to Tag Latest

--- docker_push_tag.sh
+++ docker_push_tag.sh
@@ -5,6 +5,14 @@ docker tag nextcloud-cron craftingit/nextcloud-cron:$TRAVIS_TAG
 docker tag nextcloud-mariadb craftingit/nextcloud-mariadb:$TRAVIS_TAG
 docker tag nextcloud-elasticsearch craftingit/nextcloud-elasticsearch:$TRAVIS_TAG
 
+if [ ! "$TRAVIS_TAG" == "dev" ];
+then
+    docker tag nextcloud craftingit/nextcloud:latest
+    docker tag nextcloud-cron craftingit/nextcloud-cron:latest
+    docker tag nextcloud-mariadb craftingit/nextcloud-mariadb:latest
+    docker tag nextcloud-elasticsearch craftingit/nextcloud-elasticsearch:latest
+fi
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker push craftingit/nextcloud
