f9a4aa29c9e275f4dbd152739d5c6221cbedbb73
Kai Strüssmann <Kai.Struessmann@craftingit.de>
2021-09-16 10:46:27 +0200

    Fix User/Group mod

--- Container/Nextcloud/entrypointModUser.sh
+++ Container/Nextcloud/entrypointModUser.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -z "${USER_ID}" ]; then
-    echo User: $USER_ID
-    usermod -u $USER_ID www-data
-fi
-
 if [ ! -z "${GROUP_ID}" ]; then
     echo Group: $GROUP_ID 
-    groupmod -g $GROUP_ID www-data
+    groupmod -o -g $GROUP_ID www-data
 fi
 
-sudo -u www-data /entrypoint.sh apache2-foreground
+if [ ! -z "${USER_ID}" ]; then
+    echo User: $USER_ID
+    usermod -o -u $USER_ID www-data
+fi
+
+/entrypoint.sh apache2-foreground
