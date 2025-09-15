c0d9d7130d1c1e97cfaac9b7eac6a6b6db04f6e1
Kai Strüssmann <kaistrussmann@gmail.com>
2025-09-15 19:57:54 +0200

    perfomance improvements

---
+++ Container/NextcloudCron/config/preview.config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array (
+  'preview_max_x' => 2048,
+  'preview_max_y' => 2048,
+  'preview_max_scale_factor' => 1,
+  'jpeg_quality' => 60,
+  'enable_previews' => true,
+  'enabledPreviewProviders' => array(
+    'OC\Preview\PNG',
+    'OC\Preview\JPEG',
+    'OC\Preview\GIF',
+    'OC\Preview\BMP',
+    'OC\Preview\XBitmap',
+    'OC\Preview\MP3',
+    'OC\Preview\TXT',
+    'OC\Preview\MarkDown',
+    'OC\Preview\PDF',
+    'OC\Preview\Movie',
+    'OC\Preview\MKV',
+    'OC\Preview\MP4',
+    'OC\Preview\AVI',
+  ),
+);
